d983fbba4cc7bc95491068a3a6b0e8b30f1f01d7
Leonardo <Leosinshine89@yahoo.com>
2015-11-03 02:23:52 +0800

    Signed-off-by: Leonardo <Leosinshine89@yahoo.com>

--- spab/app/controllers/TestController.php
+++ spab/app/controllers/TestController.php
@@ -12,7 +12,7 @@ class TestController extends ControllerBase{
 		//$this->view->setTemplateAfter('base');
 	}
 	public function indexAction(){
-		
+		$character=array("日","一","二","三","四","五","六");
 		$strArray=array('A','B','C','D','E','F','G','H');
 		$time=time();
 		$time=strtotime("tomorrow",$time);
@@ -43,7 +43,7 @@ class TestController extends ControllerBase{
         $objActSheet->getRowDimension('5')->setRowHeight(75);
         $objActSheet->getRowDimension('6')->setRowHeight(75);
         
-        
+        //设置居中
         for ($i=1; $i<7; $i++) { 
         	# code...
         	for ($j=0; $j <8 ; $j++) { 
@@ -52,25 +52,17 @@ class TestController extends ControllerBase{
         		$objActSheet->getStyle($strArray[$j].$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
         		$objActSheet->getStyle($strArray[$j].$i)->getAlignment()->setWrapText(true);
         	}
-        	// $objActSheet->getStyle('A'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('B'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('C'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('D'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('E'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('F'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('G'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('H'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        	// $objActSheet->getStyle('I'.$i)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-
-        	// $objActSheet->getStyle('A'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('B'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('C'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('D'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('E'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('F'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('G'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('H'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-        	// $objActSheet->getStyle('I'.$i)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+        	
+        }
+        //写入星期与日期
+        $tem=$time;
+        $index=array();
+        for ($i=0; $i <7 ; $i++) { 
+            # code...
+            $datetem=getdate($tem);
+            $objActSheet->setCellValue($strArray[$i+2]."2",$datetem['mon']."月".$datetem['mday']."日\r\rn星期".$character[$datetem['wday']]);
+            $index[$datetem['mon']."|".$datetem['mday']]=$strArray[$i+2];
+            $tem=strtotime("tomorrow",$tem);
         }
 
         $objActSheet->mergeCells('A1:I1');
@@ -84,10 +76,10 @@ class TestController extends ControllerBase{
         $objActSheet->setCellValue('B4','10-12');
         $objActSheet->setCellValue('B5','14-16');
         $objActSheet->setCellValue('B6','16-18');
-        //将测试人员信息导入到表中
+        
         
         $objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
-        $file_name = 'examinees.xls';
+        $file_name = 'schedule.xls';
         $objWriter->save($file_name);
 		 
 	}
