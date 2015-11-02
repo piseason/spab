433cda6065406dc7801ac7e7e709af1ac1e390f7
Leonardo <Leosinshine89@yahoo.com>
2015-11-02 15:23:16 +0800

    Signed-off-by: Leonardo <Leosinshine89@yahoo.com>

--- spab/app/controllers/TestController.php
+++ spab/app/controllers/TestController.php
@@ -21,20 +21,20 @@ class TestController extends ControllerBase{
          */
         $objActSheet->getColumnDimension('A')->setWidth(7);
         $objActSheet->getColumnDimension('B')->setWidth(10);
-        $objActSheet->getColumnDimension('C')->setWidth(15);
-        $objActSheet->getColumnDimension('D')->setWidth(15);
-        $objActSheet->getColumnDimension('E')->setWidth(15);
-        $objActSheet->getColumnDimension('F')->setWidth(15);
-        $objActSheet->getColumnDimension('G')->setWidth(15);
-        $objActSheet->getColumnDimension('H')->setWidth(15);
-        $objActSheet->getColumnDimension('I')->setWidth(15);
+        $objActSheet->getColumnDimension('C')->setWidth(20);
+        $objActSheet->getColumnDimension('D')->setWidth(20);
+        $objActSheet->getColumnDimension('E')->setWidth(20);
+        $objActSheet->getColumnDimension('F')->setWidth(20);
+        $objActSheet->getColumnDimension('G')->setWidth(20);
+        $objActSheet->getColumnDimension('H')->setWidth(20);
+        $objActSheet->getColumnDimension('I')->setWidth(20);
 
-        $objActSheet->getRowDimension('1')->setRowHeight(6);
-        $objActSheet->getRowDimension('2')->setRowHeight(8);
-        $objActSheet->getRowDimension('3')->setRowHeight(25);
-        $objActSheet->getRowDimension('4')->setRowHeight(25);
-        $objActSheet->getRowDimension('5')->setRowHeight(25);
-        $objActSheet->getRowDimension('6')->setRowHeight(25);
+        $objActSheet->getRowDimension('1')->setRowHeight(18);
+        $objActSheet->getRowDimension('2')->setRowHeight(24);
+        $objActSheet->getRowDimension('3')->setRowHeight(75);
+        $objActSheet->getRowDimension('4')->setRowHeight(75);
+        $objActSheet->getRowDimension('5')->setRowHeight(75);
+        $objActSheet->getRowDimension('6')->setRowHeight(75);
 
 
         $objActSheet->mergeCells('A1:I1');
@@ -44,6 +44,10 @@ class TestController extends ControllerBase{
         $objActSheet->mergeCells('A5:A6');
         $objActSheet->setCellValue('A3','上午');
         $objActSheet->setCellValue('A5','下午');
+        $objActSheet->setCellValue('B3','8-10');
+        $objActSheet->setCellValue('B4','10-12');
+        $objActSheet->setCellValue('B5','14-16');
+        $objActSheet->setCellValue('B6','16-18');
         //将测试人员信息导入到表中
         
         $objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
