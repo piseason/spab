9670aadbbb084f0be10d76e06f30428f46bf38a4
Leonardo <Leosinshine89@yahoo.com>
2015-11-04 04:39:18 +0800

    Signed-off-by: Leonardo <Leosinshine89@yahoo.com>

--- spab/public/test.php
+++ spab/public/test.php
@@ -1,3 +1,24 @@
 <?php
-	echo "hello world";
+header("Content-Type: text/html; charset=utf-8");
+include_once('CAS.php');			
+phpCAS::setDebug();												
+phpCAS::client(CAS_VERSION_2_0,'sso.buaa.edu.cn',443,'');
+phpCAS::setNoCasServerValidation();
+phpCAS::forceAuthentication();
+phpCAS::handleLogoutRequests();  
+$auth = phpCAS::checkAuthentication();
+if($auth){
+	$username = phpCAS::getAttributes();
+	//echo $username['uid'];
+	//$_SESSION["auth"]=$username;
+	//header("Location: http://".$localurl.":5001/index/apply");   
+	exit;
+} 
+
+
+
+
+ //当前的登录用户ID
+//如果有扩展属性 可以参考的到用户ID的方法如$userNo= phpCAS::getAttribute("cn");前提是cas客户段已经配置了扩展属性
 ?>
+
