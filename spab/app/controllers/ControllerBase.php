4c5beb41c67557077c902e78f577adec31474a23
Leonardo <Leosunshine89@gmail.com>
2016-04-13 20:18:28 +0800

    Signed-off-by: Leonardo <Leosunshine89@gmail.com>

--- spab/app/controllers/ControllerBase.php
+++ spab/app/controllers/ControllerBase.php
@@ -15,13 +15,14 @@ class ControllerBase extends \Phalcon\Mvc\Controller
 	if($times){
 		return ($times->newstate=='休息日');
 	}
-	if($date==6||$date==7){
+	if($date==6||$date==0){
 		return true;
 	}
 
 	$str_trim=split("周", $str);
 	$str_trim=split(":", $str_trim[1]);
 	$str_trim=substr($str_trim[0], 1);
+	echo $str_trim;
 	if($date==4&&($str_trim=="14"||$str_trim=="16")){
 		return true;
 	}
